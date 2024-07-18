9c34295ef487d3b11d489109b4d2e597a7a84693
Ujfalusi.Abel <abelujfalusi4@gmail.com>
2024-07-18 23:08:56 +0300

    Corrected problem of unincluded .cmake files

---
+++ cmake/Warnings.cmake
@@ -0,0 +1,25 @@
+function(set_compiler_warnings TARGET AS_ERRORS)
+    set(GCC_WARNINGS -Wall -Wextra)
+    set(CLANG_WARNINGS ${GCC_WARNINGS})
+    set(MSVC_WARNINGS /W4 /permissive-)
+
+    if(${ENABLED_AS_ERRORS})
+        set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
+        set(CLANG_WARNINGS ${GCC_WARNINGS})
+        set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+    endif()
+
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        set(WARNINGS ${GCC_WARNINGS})
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        set(WARNINGS ${CLANG_WARNINGS})
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+        set(WARNINGS ${MSVC_WARNINGS})
+    else()
+        message(ERROR "Unsupported compiler. Please use GCC, Clang or MSVC")
+    endif()
+
+    target_compile_options(${TARGET} PRIVATE ${WARNINGS})
+    message(STATUS ${WARNINGS})
+
+endfunction()
